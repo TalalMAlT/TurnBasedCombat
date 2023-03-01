2665fa758453841ff900c9bcb3e39a67d3142de9
Biro1Games <akhan0088@stu.kau.edu.sa>
2023-03-01 10:07:08 +0300

    update move

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMoveController : MonoBehaviour
 {
-    [SerializeField] private RectTransform playerRectTransform;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] private RectTransform playerRectTransform;
+
+    // physical inputs
     void Update()
     {
+        var position = playerRectTransform.anchoredPosition;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            position.x += 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            position.x -= 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            position.y += 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            position.y -= 1.0f;
+        }
+        playerRectTransform.anchoredPosition = position;
     }
+
+   // public class PlayerCollision : MonoBehaviour
+    //{
+        private void OnTriggerEnter2D(Collider2D col)
+        
+        {
+            if (col.gameObject.CompareTag("Enemy1"))
+            {
+                SceneManager.LoadScene("MainGame");
+            }
+        }
+
+
+    //}
 }
